1c9e745dcf9f838ae8b1656fdbfe5db523f3487c
Quinn Langille <quinn@quinnlangille.com>
2022-06-08 20:05:30 -0400

    feat: remove technical coach

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ const IndexPage = () => (
   <Layout>
     <h1 className="title">{ config.title }</h1>
     <p> Software Developer @ SSENSE </p>
-    <p> Technical Coach @ Journey Education </p>
     <br />
     <a href="mailto:me@quinnlangille.com" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>email</a>
   </Layout>
